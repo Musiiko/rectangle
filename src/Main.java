7910de15533783789f97ff72d1fdeaf0280f7bf4
MSP1981 <99791179+MSP1981@users.noreply.github.com>
2022-07-02 13:30:49 +0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner arearectangle = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("Enter the length of the rectangle = ");
+        double length = arearectangle.nextDouble();
+
+        System.out.print("Enter the width of the rectangle = ");
+        double width = arearectangle.nextDouble();
+
+        double area = length * width;
+        System.out.printf("AREA = " + "%.2f", area);
+
     }
 }
